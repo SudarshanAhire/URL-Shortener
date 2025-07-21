b6446c8ff44dbe892b485f5f7084de661fdc1d28
SudarshanAhire <sudarshanahire292@gmail.com>
2025-07-21 17:15:57 +0530

    initial commit

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -77,11 +77,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimrr.in/${link}`}
+            href={`https://https://url-shortener-seven-rouge.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimrr.in/{link}
+            https://https://url-shortener-seven-rouge.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -98,7 +98,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimrr.in/${link}`)
+                navigator.clipboard.writeText(`https://https://url-shortener-seven-rouge.vercel.app/${link}`)
               }
             >
               <Copy />
